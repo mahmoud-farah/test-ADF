094b00d55a73c2d020f3bdb83f4cb3ee0e6354dc
mahmoud-farah <139159072+mahmoud-farah@users.noreply.github.com>
2024-09-09 09:10:19 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,15 @@
+variable "subscription_id" {
+  description = "The subscription ID for Azure"
+  type        = string
+}
+
+variable "client_id" {
+  description = "The client ID for Azure"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "The tenant ID for Azure"
+  type        = string
+}
 
