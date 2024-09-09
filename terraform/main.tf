d2e9c4e9e4eb835aadc2e8900e0e9b55f7c6602d
mahmoud-farah <139159072+mahmoud-farah@users.noreply.github.com>
2024-09-09 15:21:22 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,9 +2,8 @@ provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
   client_id       = var.client_id
+  client_secret   = var.client_secret
   tenant_id       = var.tenant_id
-  use_msi         = true
-  msi_client_id    = "ab978546-8235-452e-9239-414c360d126c"
 }
 
 resource "azurerm_resource_group" "prod" {
